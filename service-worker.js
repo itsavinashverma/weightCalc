8eff52238cbeefb2797bebd5dfee06b6caac0348
Avinash Verma <162625575+itsavinashverma@users.noreply.github.com>
2025-12-26 14:18:15 +0530

    Update service worker cache version and logic

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,43 @@
-const CACHE_NAME = 'weight-calculator-cache-v2';
+const CACHE_NAME = 'weight-calc-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icon-192x192.png',
-    './icon-512x512.png'
-    // removed style.css and script.js as they are likely bundled now
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+  // Note: If you have a separate script.js or style.css, add them here.
+  // Since you are using React, you likely have a build file (e.g., main.jsx or bundle.js)
 ];
 
-// Install the service worker
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// Fetch cached resources
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached version or fetch new
+      return response || fetch(event.request);
+    })
+  );
 });
 
-// Activate - Clean up old caches
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
